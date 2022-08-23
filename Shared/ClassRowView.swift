9d33035bf173fc76d3b6b52972b2c77e23a0f01b
Jason Burke <burkejasonj@icloud.com>
2022-08-23 00:51:06 -0700

    Added explicit initializer

--- Shared/ClassRowView.swift
+++ Shared/ClassRowView.swift
@@ -2,28 +2,55 @@ import SwiftUI
 
 struct ClassRowView: View {
     var classInfo: Class?
+
+    var accentColor: Color
+    var title: String
+    var level: String
     
-    var accentColor: Color {
-        Color(hex: (classInfo?.color) ?? "248ace") ?? Color.accentColor
-    }
-    
-    var title: String {
-        classInfo?.name ?? "Unknown Class"
-    }
-    var level: String {
-        classInfo?.level ?? ""
-    }
-    var teacher: String {
-        classInfo?.teacher?.name ?? "Unknown Instructor"
-    }
-    var creator: String {
-        classInfo?.creator?.name ?? "Unknown Provider"
-    }
+    var teacher: String
+    var creator: String
     
     let scale: CGFloat = 1
     
-    var gradient: Gradient {
-        Gradient(colors: [accentColor, Color.clear])
+    var gradient: Gradient
+    
+    init() {
+        self.accentColor = Color("248ace")
+        self.title = "Unknown Class"
+        self.level = ""
+        
+        self.teacher = "Unknown Instructor"
+        self.creator = "Unknown Provider"
+        
+        self.gradient = Gradient(colors: [accentColor, Color.clear])
+        
+    }
+    
+    init(classInfo: Class?) {
+        self.classInfo = classInfo
+        
+        self.accentColor = Color(hex: (classInfo?.color) ?? "248ace") ?? Color.accentColor
+        self.title = classInfo?.name ?? "Unknown Class"
+        self.level = classInfo?.level ?? ""
+        
+        self.teacher = classInfo?.teacher?.name ?? "Unknown Instructor"
+        self.creator = classInfo?.creator?.name ?? "Unknown Provider"
+        
+        self.gradient = Gradient(colors: [accentColor, Color.clear])
+        
+    }
+    
+    init(accentColor: Color?, title: String, level: String) {
+        self.classInfo = nil
+        
+        self.accentColor = accentColor ?? Color.accentColor
+        self.title = title
+        self.level = level
+        
+        self.teacher = "Unknown Instructor"
+        self.creator = "Unknown Provider"
+        
+        self.gradient = Gradient(colors: [self.accentColor, Color.clear])
     }
     
     var body: some View {
