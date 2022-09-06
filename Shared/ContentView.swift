56f9706a9d9c14d90043575959fe592cb919f20d
Jason Burke <burkejasonj@icloud.com>
2022-09-05 21:30:49 -0700

    Re-add Fix All for development

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -46,6 +46,13 @@ struct ContentView: View {
             }
             .accentColor(.init(UIColor.secondarySystemFill))
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        UserDefaults.standard.set(false, forKey: "isPreloaded")
+                    } label: {
+                        Text("Fix All")
+                    }
+                }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
