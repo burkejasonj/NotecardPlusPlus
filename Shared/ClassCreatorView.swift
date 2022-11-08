bff4497a948bf82430b72cf45895ced25130829e
Jason Burke <burkejasonj@icloud.com>
2022-11-07 19:53:35 -0800

    Reimplement creator for macOS

--- Shared/ClassCreatorView.swift
+++ Shared/ClassCreatorView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+#if os(macOS)
 struct ClassCreatorView: View {
     @Environment(\.dismiss) var dismiss
     
@@ -13,33 +14,113 @@ struct ClassCreatorView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
+            Group {
+                Form {
+                    TextField("Name:",
+                              text: $classNameString)
+                    .focused($focusState, equals: "name")
+                    .onSubmit {
+                        focusState = "level"
+                    }
+                    TextField("Level:",
+                              text: $classLevelString)
+                    .focused($focusState, equals: "level")
+                    .onSubmit {
+                        focusState = "year"
+                    }
+                    TextField("Year:",
+                              text: $classYearInt.value)
+                    .focused($focusState, equals: "year")
+                    ColorPicker(
+                        "Color:",
+                        selection: $classColor,
+                        supportsOpacity: false
+                    )
+                    ClassRowView(
+                        accentColor: classColor,
+                        title: classNameString,
+                        level: classLevelString
+                    )
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Cancel")
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button {
+                        let thisClass = Class(context: viewContext)
+                        
+                        thisClass.name = classNameString
+                        thisClass.level = classLevelString
+                        thisClass.color = classColor.toHex()
+                        thisClass.uuid = UUID()
+                        thisClass.year = Int64(classYearInt.value) ?? 2022
+                        thisClass.order = 0
+                        
+                        do {
+                            try viewContext.save()
+                        } catch {
+                            print("error")
+                        }
+                        
+                        dismiss()
+                    } label: {
+                        Text("Create")
+                    }
+                }
+            }
+            .navigationTitle("Create Class")
+        }
+        .padding()
+    }
+}
+#else
+struct ClassCreatorView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State var classNameString = ""
+    @State var classLevelString = ""
+    @ObservedObject var classYearInt = NumberInput()
+    @State var classColor: Color = .accentColor
+    
+    @FocusState var focusState: String?
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    var body: some View {
+        NavigationStack {
             Group {
                 Form {
                     Section("ATTRIBUTES") {
-                        TextField("Name",
+                        TextField("",
                                   text: $classNameString,
-                                  prompt: Text("Class Name"))
+                                  prompt: Text("Name"))
                             .focused($focusState, equals: "name")
                             .onSubmit {
                                 focusState = "level"
                             }
-                        TextField("Level",
+                        TextField("",
                                   text: $classLevelString,
-                                  prompt: Text("Class Level"))
+                                  prompt: Text("Level"))
                             .focused($focusState, equals: "level")
                             .onSubmit {
                                 focusState = "year"
                             }
-                        TextField("Year",
+                        TextField("",
                                   text: $classYearInt.value,
-                                  prompt: Text("Class Year"))
+                                  prompt: Text("Year"))
 #if os(iOS)
                             .keyboardType(.numberPad)
 #endif
                             .focused($focusState, equals: "year")
                         ColorPicker(selection: $classColor, supportsOpacity: false) {
-                            Text("Class Color")
+                            Text("Color")
                         }
                     }
                     Section("PREVIEW") {
@@ -88,6 +169,7 @@ struct ClassCreatorView: View {
         }
     }
 }
+#endif
 
 struct ClassCreatorView_Previews: PreviewProvider {
     static var previews: some View {
